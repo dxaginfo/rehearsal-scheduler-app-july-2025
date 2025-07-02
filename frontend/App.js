c02767d7836179955b2547218dc3b5991ae32b89
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-07-01 23:32:22 -0300

    Initial React app entry point

---
+++ frontend/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', margin: 10 }}>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Band management | Schedule rehearsals | Calendar | Reminders</p>
+      <p>Mobile responsive (Try resizing the window!)</p>
+    </div>
+  );
+}
+export default App;
